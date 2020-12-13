c3d6e2d2f7776b9942dd0a1f7cff719a48f0cdca
cjh1249131356 <cjh1249131356@gmail.com>
2020-12-13 15:33:53 +0800

    kebab: Evolution-X bringup

--- evolution_kebab.mk
+++ evolution_kebab.mk
@@ -21,10 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kebab device
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kebab
+WITH_GAPPS := true
+EXTRA_FOD_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := evolution_kebab
 PRODUCT_DEVICE := kebab
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
