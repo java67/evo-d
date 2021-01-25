942e076b235997cdd9141844c502785b777994c4
cjh1249131356 <cjh1249131356@gmail.com>
2021-01-25 22:21:11 +0800

    Revert "instantnoodlep: Build with Redfin Jan 2021 fingerprint"
    This reverts commit e9ea24177f384fa05d6a50f6c173186627e64116.

--- evolution_instantnoodlep.mk
+++ evolution_instantnoodlep.mk
@@ -41,4 +41,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus8Pro \
     PRIVATE_BUILD_DESC="OnePlus8Pro-user 10 QKQ1.191222.002 2005301332 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2005301332:user/release-keys
